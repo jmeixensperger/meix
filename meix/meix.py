2a7ec3b7a4e3ae8c7dff7c1ffeef7ec56b49c033
jmeixensperger <jmeixensperger@gmail.com>
2018-07-31 16:04:05 +0800

    another tox flake8 fix

--- meix/meix.py
+++ meix/meix.py
@@ -5,6 +5,6 @@
 
 def message(name):
     """Some function for testing pypi package building"""
-	if name == "":
-		name = "pretend person"
-	print("Hello {usr_name}! Welcome to Shanghai!".format(usr_name=name))
+    if name == "":
+        name = "pretend person"
+    print("Hello {usr_name}! Welcome to Shanghai!".format(usr_name=name))
